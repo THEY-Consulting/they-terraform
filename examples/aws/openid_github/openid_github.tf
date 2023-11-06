5142a79a2529372ac3451ee5425a8b4eb66265ab
Thiemo <thiemo.gries@they-consulting.de>
2023-11-06 12:25:18 +0100

    moved openid examples to their own example folders (#26)

--- examples/aws/openid_github/openid_github.tf
+++ examples/aws/openid_github/openid_github.tf
@@ -2,7 +2,7 @@
 
 module "github_action_role" {
   # source = "github.com/THEY-Consulting/they-terraform//aws/openid/github"
-  source = "../../aws/openid/github"
+  source = "../../../aws/openid/github"
 
   name = "they-test"
   repo = "THEY-Consulting/they-terraform"
