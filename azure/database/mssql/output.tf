f204ea0cd3282272b9d69298df0533c4dfb7fcef
Thiemo <thiemo.gries@they-consulting.de>
2023-11-28 09:06:51 +0100

    add ADO.NET connection string
    Co-authored-by: Pierre Dahmani <hi@pierre-dev.com>

--- azure/database/mssql/output.tf
+++ azure/database/mssql/output.tf
@@ -11,5 +11,14 @@ output "server_domain_name" {
 }
 
 output "ODBC_connection_string" {
+  value       = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:${data.azurerm_mssql_server.main.fully_qualified_domain_name},1433;Database=${azurerm_mssql_database.main.name};Uid=${data.azurerm_mssql_server.main.administrator_login};Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+  description = "Can be used to connect to database via ODBC. Used in nodejs"
+}
+
+output "ADONET_connection_string" {
+  value       = "Server=tcp:${data.azurerm_mssql_server.main.fully_qualified_domain_name},1433;Database=${azurerm_mssql_database.main.name};Uid=${data.azurerm_mssql_server.main.administrator_login};Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+  description = "Can be used to connect to database via azure data studio."
+}
+
   value = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:${data.azurerm_mssql_server.main.fully_qualified_domain_name},1433;Database=${azurerm_mssql_database.main.name};Uid=${data.azurerm_mssql_server.main.administrator_login};Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 }
