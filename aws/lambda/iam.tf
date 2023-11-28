afd77cd4f14d3565e3dd9c24abedc02bef5d97ea
Pierre Dahmani <hi@pierre-dev.com>
2023-11-28 14:38:46 +0100

    Update aws/lambda/iam.tf
    Co-authored-by: Thiemo <thiemo.gries@they-consulting.de>

--- aws/lambda/iam.tf
+++ aws/lambda/iam.tf
@@ -47,7 +47,13 @@ resource "aws_iam_role" "role" {
         Version = "2012-10-17"
         Statement = [{
           Effect   = "Allow"
-          Action   = ["ec2:CreateNetworkInterface", "ec2:DescribeNetworkInterfaces", "ec2:DeleteNetworkInterface", "ec2:AssignPrivateIpAddresses", "ec2:UnassignPrivateIpAddresses"]
+          Action   = [
+            "ec2:CreateNetworkInterface", 
+            "ec2:DescribeNetworkInterfaces", 
+            "ec2:DeleteNetworkInterface", 
+            "ec2:AssignPrivateIpAddresses", 
+            "ec2:UnassignPrivateIpAddresses"
+          ]
           Resource = "*"
         }]
       })
