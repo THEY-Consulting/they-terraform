ace1e8f00cd5129c2a57b5cabb5300fa1709062f
Eduardo Rodriguez <erodrigufer@gmail.com>
2023-12-12 18:45:30 +0100

    Remove terraform.workspace from name
    Co-authored-by: Thiemo <thiemo.gries@they-consulting.de>

--- aws/auto-scaling-group/application-load-balancer.tf
+++ aws/auto-scaling-group/application-load-balancer.tf
@@ -21,7 +21,7 @@ resource "aws_lb_listener" "lb_listener" {
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "${terraform.workspace}-${var.name}-tg"
+  name     = "${var.name}-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc.id
