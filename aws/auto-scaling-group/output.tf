b3b6c23e440504ba7f51ef1f1154a6f8ed06146b
Thiemo <thiemo.gries@they-consulting.de>
2024-06-04 14:10:28 +0200

    include nat gateway public ips in asg module output

--- aws/auto-scaling-group/output.tf
+++ aws/auto-scaling-group/output.tf
@@ -7,3 +7,8 @@ output "alb_zone_id" {
   description = "Zone ID of the application load balancer."
   value       = var.loadbalancer_disabled ? null : aws_lb.lb[0].zone_id
 }
+
+output "nat_gateway_ips" {
+  description = "Public IPs of the NAT gateways."
+  value       = aws_nat_gateway.natgw.*.public_ip
+}
