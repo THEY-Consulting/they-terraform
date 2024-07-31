5e209b740f8c299c1df2d2c80ffbb40637e744f2
erodrigufer <erodrigufer@gmail.com>
2024-07-31 15:30:24 +0200

    Add resource group name to DNS zone
    - The CD pipeline returns an error because it cannot find the DNS zone,
    maybe this could fix that.

--- azure/container-instances/domain.tf
+++ azure/container-instances/domain.tf
@@ -1,7 +1,9 @@
 data "azurerm_dns_zone" "dns_zone" {
   count = var.dns_zone_name != null ? 1 : 0
   name =  var.dns_zone_name 
+  resource_group_name = var.dns_resource_group
 }
+
 resource "azurerm_dns_a_record" "dns_a_record" {
   count               = var.dns_zone_name != null ? 1 : 0
   name                = var.dns_a_record_name
