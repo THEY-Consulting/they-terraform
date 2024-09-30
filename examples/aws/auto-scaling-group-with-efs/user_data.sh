a4c032de75e9e2fff7eebc68a9d36cc8c4b4ae0c
Thiemo <thiemo.gries@they-consulting.de>
2024-09-30 16:08:52 +0200

    disable automount in example for now

--- examples/aws/auto-scaling-group-with-efs/user_data.sh
+++ examples/aws/auto-scaling-group-with-efs/user_data.sh
@@ -9,7 +9,7 @@ mkdir /mnt/efs
 # Mount the EFS file system
 # https://docs.aws.amazon.com/efs/latest/ug/mounting-fs-mount-helper-ec2-linux.html
 # TODO: use variable for EFS ID and region
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport FILE_SYSYEM_ID.efs.AWS_REGION.amazonaws.com:/ /mnt/efs
+# mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport FILE_SYSYEM_ID.efs.AWS_REGION.amazonaws.com:/ /mnt/efs
 
 # Respond with a webpage with the private IP of the instance on port 80.
 mkdir /var/www
