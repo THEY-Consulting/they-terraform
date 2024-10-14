495b68850b9d412a23a93084e6652974755dcb34
Thiemo <thiemo.gries@they-consulting.de>
2024-10-14 11:34:44 +0200

    Feature/efs (#102)
    * add option to mount efs within aws lambdas
    
    * option to provide existing vpc to asg
    
    * option to attach extra ebs volumes to asg
    
    * add efs examples
    
    * add option to use a single availability zone for an asg
    
    * replace deprecated inline_policy argument
    
    * chore: add terrform lockfile after init upgrade
    
    ---------
    
    Co-authored-by: Merle Fischer <mer.fischer@gmail.com>

--- aws/auto-scaling-group/application-load-balancer.tf
+++ aws/auto-scaling-group/application-load-balancer.tf
@@ -63,7 +63,7 @@ resource "aws_lb_target_group" "tg" {
   name     = var.name
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.vpc.id
+  vpc_id   = local.vpc_id
 
   health_check {
     path = var.health_check_path
