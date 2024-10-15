852422f6beedfc97554f49c7971b7580e9dab18d
Merle Fischer <mer.fischer@gmail.com>
2024-10-15 10:25:05 +0200

    Revert "Revert "Feature/efs (#102)" (#106)" (#107)
    This reverts commit d2ed38aa356c38a881de1d68ffd24471f63277d9.

--- aws/auto-scaling-group/output.tf
+++ aws/auto-scaling-group/output.tf
@@ -12,3 +12,18 @@ output "nat_gateway_ips" {
   description = "Public IPs of the NAT gateways."
   value       = aws_nat_gateway.natgw.*.public_ip
 }
+
+output "security_group_id" {
+  description = "ID of the security group."
+  value       = aws_security_group.sg.id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets."
+  value       = aws_subnet.instances_subnets.*.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets."
+  value       = aws_subnet.alb_public_subnets.*.id
+}
