5e7e98a1ab9f4ac31ee32b39bcde26013328b038
martprog <martinscaraguel@gmail.com>
2025-02-06 12:22:32 +0100

    refactor: simplify app URL mapping logic based on DNS zone and record type

--- azure/container-apps/output.tf
+++ azure/container-apps/output.tf
@@ -1,15 +1,13 @@
 locals {
-  url_mapping = {
-    no_dns_zone = { for app in azurerm_container_app.container_app : app.name => app.latest_revision_fqdn }
-    a_record    = { for app_name, app in var.container_apps : app_name => var.dns_zone.existing_dns_zone_name }
-    cname       = { for app_name, app in var.container_apps : app_name => "${app.subdomain}.${var.dns_zone.existing_dns_zone_name}" }
+  app_urls = { for app_name, app in var.container_apps : app_name =>
+    var.dns_zone == null ? azurerm_container_app.container_app[app_name].latest_revision_fqdn : (
+      var.use_a_record == true ? var.dns_zone.existing_dns_zone_name : "${app.subdomain}.${var.dns_zone.existing_dns_zone_name}"
+    )
   }
-
-  url_type = var.dns_zone == null ? "no_dns_zone" : (var.use_a_record ? "a_record" : "cname")
 }
 
 output "container_apps_urls" {
-  value = local.url_mapping[local.url_type]
+  value = local.app_urls
 }
 # output "public_ip" {
 # value = azurerm_container_app_environment.app_environment.static_ip_address
