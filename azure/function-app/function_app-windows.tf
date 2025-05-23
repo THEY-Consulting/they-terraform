e91fdb6f87b9dbb9012d524c1ff0e81d4168396d
voidwaves <incognitoxyx@googlemail.com>
2025-05-23 16:51:50 +0200

    minor change

--- azure/function-app/function_app-windows.tf
+++ azure/function-app/function_app-windows.tf
@@ -49,7 +49,7 @@ resource "azurerm_windows_function_app" "function_app" {
 
   site_config {
     application_insights_key = var.insights.enabled ? azurerm_application_insights.app_insights.0.instrumentation_key : null
-    # vnet_route_all_enabled   = var.needs_mdm_access
+    vnet_route_all_enabled   = var.needs_mdm_access
 
     ip_restriction {
       virtual_network_subnet_id = azurerm_subnet.subnet.0.id
