ccdf7eee58cf7b05b302c48249c7ee9b67a9ba49
Marten Kiehn <marten.kiehn@they-consulting.de>
2026-01-21 13:58:15 +0100

    x-aws-waf-token

--- aws/lambda/gateway/integration.tf
+++ aws/lambda/gateway/integration.tf
@@ -85,7 +85,7 @@ resource "aws_api_gateway_integration_response" "options" {
   response_templates = { "application/json" = "" }
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,aws-waf-token'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,x-aws-waf-token'"
     "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
